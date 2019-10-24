bf73b1f54de06acc5fd1d8cbf6a12fb6bfbb3559
Ocean Ho <ocean@oceanho.com>
2019-10-24 13:27:13 +0800

    Create gitPS1.sh

---
+++ docs/git/script/gitPS1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# A git extension script enhancement of PS1
+# Created By OceanHo (gzhehai@foxmail.com) AT 2019/10/24
+
+# retrieveGitCurrentBranch retrieve a git branch and working tree status of currentlly directory
+function retrieveGitCurrentBranch(){
+	local str=$(git branch 2>/dev/null | awk '$0 ~ /[*]/ {print $2}')
+	if [ "$str" != "" ]; then
+		local wt=$(git status | tail -n 1 | awk '$0 ~ /nothing to commit, working tree clean/ {print "clean"}')
+		if [ "$wt" == "clean" ]; then
+			# Working tree are clean, green color
+			str="\e[1;32m($str)\e[m"
+		else
+			# Working tree are clean, red color
+			str="\e[0;31m($str)\e[m"
+		fi
+		echo -e "$str"
+	fi
+}
+# reference: http://bashrcgenerator.com/
+export PS1="\[\033[38;5;12m\]\u@\h:\[$(tput sgr0)\]\[\033[38;5;6m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]\$(retrieveGitCurrentBranch)\$ \[$(tput sgr0)\]"
